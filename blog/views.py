ba877e0f2e8318bae752bebac6bb3c85045e2bc5
rafsun-riad <mruhaquer@gmail.com>
2024-03-05 19:40:49 +0600

    fixed view

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datetime import date
 from .models import Tag, Author, Post
 
 # Create your views here.
 
-posts_list = [
-
-]
-
-
-def get_date(post):
-    return post["date"]
-
 
 def starting_page(request):
-    sorted_posts = sorted(posts_list, key=get_date)
-    latest_post = sorted_posts[-3:]
+    latest_post = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {"posts": latest_post})
 
 
 def posts(request):
-    return render(request, "blog/all-posts.html", {"all_posts": posts_list})
+    all_posts = Post.objects.all().order_by("-date")
+    return render(request, "blog/all-posts.html", {"all_posts": all_posts})
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in posts_list if post["slug"] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {"post": identified_post})
