2a54c89708508896ba8d6f04123d646f8e584a92
rafsun-riad <mruhaquer@gmail.com>
2024-04-04 00:14:27 +0600

    added comment form styling and valid

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from datetime import date
+from django.urls import reverse
 from .models import Tag, Author, Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from .forms import CommentForm
+from django.views import View
 # Create your views here.
 
 
@@ -25,12 +28,30 @@ class AllPostView(ListView):
     context_object_name = "all_posts"
 
 
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class SinglePostView(View):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm
+        }
+        return render(request, "blog/post-detail.html", context)
